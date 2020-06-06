c0246a96d2f4d07f7c7bf137fcb1907fe5ee6382
张 哲城 <junguan_quanli@outlook.com>
2020-06-06 10:19:25 +0800

    Using Panuon to develop

--- Huya_Offical/Classes/LiveRooms/Build/LiveRooms.cs
+++ Huya_Offical/Classes/LiveRooms/Build/LiveRooms.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Panuon.UI.Silver;
+using Panuon.UI.Silver.Core;
+using System.Windows.Media;
+
 namespace Huya_Offical.Classes.LiveRooms.Build
 {
     public class LiveRooms
@@ -13,6 +17,9 @@ namespace Huya_Offical.Classes.LiveRooms.Build
         const double FREESPACE = 10;
         const double HEIGHT = 190;
         const double WIDTH = 338;
+        const string COLORARGB = "FF3F3F3F";
+        const double CORNERRADIUS = 6.0;
+        const double SPACING = 5.0;
         //public struct BuildImagesInformation
         //{
             //public int HorizonLength;
@@ -28,10 +35,18 @@ namespace Huya_Offical.Classes.LiveRooms.Build
             var LiveRoomInformation = get.PageInformation(1);//接口设计失误，先执行以获得总页数
             var TotalPages = LiveRoomInformation.TotalPage;
             var PageSize = LiveRoomInformation.PageSize;
-            for(int i = 1;i <= TotalPages;i++)
+            BrushConverter brushConverter = new BrushConverter();
+            Brush brush = (Brush)brushConverter.ConvertFromString(COLORARGB);
+            CornerRadius cornerRadius = new CornerRadius(CORNERRADIUS);
+            //Brush brush = new SolidColorBrush(Color.FromArgb(1, 1, 1, 1));
+            Pagination pagination = new Pagination()
             {
+                Height = 28,
+                HoverBrush = brush,
+                CornerRadius = cornerRadius,
+                Spacing = SPACING,
 
-            }
+            };
         }
     }
 }
