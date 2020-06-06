dcad4d8362c2bc3bdc6780accf4ba41c6c4fcbc3
张 哲城 <junguan_quanli@outlook.com>
2020-06-06 10:28:05 +0800

    Added a new routine for BuildPaginition

--- Huya_Offical/Classes/LiveRooms/Build/LiveRooms.cs
+++ Huya_Offical/Classes/LiveRooms/Build/LiveRooms.cs
@@ -20,6 +20,7 @@ namespace Huya_Offical.Classes.LiveRooms.Build
         const string COLORARGB = "FF3F3F3F";
         const double CORNERRADIUS = 6.0;
         const double SPACING = 5.0;
+        
         //public struct BuildImagesInformation
         //{
             //public int HorizonLength;
@@ -31,22 +32,53 @@ namespace Huya_Offical.Classes.LiveRooms.Build
         //}
         private string GetLiveRoomInformation()
         {
-            Get.Get get = new Get.Get();
-            var LiveRoomInformation = get.PageInformation(1);//接口设计失误，先执行以获得总页数
-            var TotalPages = LiveRoomInformation.TotalPage;
-            var PageSize = LiveRoomInformation.PageSize;
-            BrushConverter brushConverter = new BrushConverter();
-            Brush brush = (Brush)brushConverter.ConvertFromString(COLORARGB);
-            CornerRadius cornerRadius = new CornerRadius(CORNERRADIUS);
-            //Brush brush = new SolidColorBrush(Color.FromArgb(1, 1, 1, 1));
-            Pagination pagination = new Pagination()
+            try
             {
-                Height = 28,
-                HoverBrush = brush,
-                CornerRadius = cornerRadius,
-                Spacing = SPACING,
-
-            };
+                Get.Get get = new Get.Get();
+                var LiveRoomInformation = get.PageInformation(1);//接口设计失误，先执行以获得总页数
+                var TotalPages = LiveRoomInformation.TotalPage;
+                var PageSize = LiveRoomInformation.PageSize;
+                BrushConverter brushConverter = new BrushConverter();
+                Brush brush = (Brush)brushConverter.ConvertFromString(COLORARGB);
+                CornerRadius cornerRadius = new CornerRadius(CORNERRADIUS);
+                //Brush brush = new SolidColorBrush(Color.FromArgb(1, 1, 1, 1));
+                Pagination pagination = new Pagination()
+                {
+                    Height = 28,
+                    HoverBrush = brush,
+                    CornerRadius = cornerRadius,
+                    Spacing = SPACING,
+                    TotalIndex = TotalPages
+                };
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("在构建分页时，遇到错误：" + e.Message);
+                return null;
+            }
+            
+        }
+        private bool BuildPaginition()
+        {
+            try
+            {
+                Get.Get get = new Get.Get();
+                var LiveRoomInformation = get.PageInformation(1);//接口设计失误，先执行以获得总页数
+                var TotalPages = LiveRoomInformation.TotalPage;
+                var PageSize = LiveRoomInformation.PageSize;
+                BrushConverter brushConverter = new BrushConverter();
+                Brush brush = (Brush)brushConverter.ConvertFromString(COLORARGB);
+                CornerRadius cornerRadius = new CornerRadius(CORNERRADIUS);
+                //Brush brush = new SolidColorBrush(Color.FromArgb(1, 1, 1, 1));
+                Pagination pagination = new Pagination()
+                {
+                    Height = 28,
+                    HoverBrush = brush,
+                    CornerRadius = cornerRadius,
+                    Spacing = SPACING,
+                    TotalIndex = TotalPages
+                };
+            }
         }
     }
 }
